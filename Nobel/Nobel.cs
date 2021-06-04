b4196c8e515eaffbc95bde539af447c95ab7a409
zeon77 <zeon77@gmail.com>
2021-06-04 13:36:40 +0200

    2. feladat

--- Nobel/Nobel.cs
+++ Nobel/Nobel.cs
@@ -10,6 +10,14 @@ namespace Nobel
     {
         static void Main(string[] args)
         {
+            //2.
+            List<NobelDíj> nobelDíjak = new List<NobelDíj>();
+            foreach (var sor in File.ReadAllLines("nobel.csv").Skip(1))
+            {
+                nobelDíjak.Add(new NobelDíj(sor));
+            }
+
+            Console.WriteLine($"Sorok száma: {nobelDíjak.Count}");
 
             Console.ReadKey();
         }
---
+++ Nobel/NobelDíj.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nobel
+{
+    class NobelDíj
+    {
+        public enum NobelDíjTípus
+        {
+            fizikai,
+            kémiai,
+            orvosi,
+            irodalmi,
+            béke,
+            közgazdaságtani
+        }
+
+        public int Évszám { get; set; }
+        public NobelDíjTípus Típus { get; set; }
+        public string Keresztnév { get; set; }
+        public string Vezetéknév { get; set; }
+
+        public NobelDíj(string sor)
+        {
+            string[] s = sor.Split(';');
+            Évszám = int.Parse(s[0]);
+            Enum.TryParse(s[1], out NobelDíjTípus nt);
+            Típus = nt;
+            Keresztnév = s[2];
+            Vezetéknév = s[3];
+        }
+    }
+}
